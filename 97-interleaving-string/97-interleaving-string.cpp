33df41bacb795e4b473e3d1583d08cbe76ec7cdb
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-04-03 11:33:06 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (88.90%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,52 +1,21 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int l1=s1.length();
-        int l2=s2.length();
-        int l3=s3.length();
-        if(l3!=l1+l2) return 0;
-        bool dp[l3+1][l1+1][l2+1];
-        for(int i=0;i<=l3;i++){
-            for(int j=0;j<=l1;j++){
-                for(int k=0;k<=l2;k++) dp[i][j][k]=0;
-            }
-        }
-        for(int i=0;i<=l3;i++){
-            for(int j=0;j<=i&&j<=l1;j++){
-                for(int k=0;k+j<=i&&k<=l2;k++){
-                    if(i==0&&j==0&&k==0){
-                        dp[0][0][0]=1;
-                    }
-                    else if(j==0&&k==0){
-                        dp[i][j][k]=0;
-                    }
-                    else if(j==0){
-                        if(s3[i-1]==s2[k-1]){
-                            dp[i][j][k]=dp[i-1][j][k-1];
-                        }
-                        else dp[i][j][k]=0;
-                    }
-                    else if(k==0){
-                        if(s3[i-1]==s1[j-1]){
-                            dp[i][j][k]=dp[i-1][j-1][k];
-                        }
-                        else dp[i][j][k]=0;
-                    }
-                    else{
-                        if(s3[i-1]==s1[j-1]&&s3[i-1]==s2[k-1]){
-                            dp[i][j][k]=dp[i-1][j-1][k]|dp[i-1][j][k-1];
-                        }
-                        else if(s3[i-1]==s1[j-1]){
-                            dp[i][j][k]=dp[i-1][j-1][k];
-                        }
-                        else if(s3[i-1]==s2[k-1]){
-                            dp[i][j][k]=dp[i-1][j][k-1];
-                        }
-                        else dp[i][j][k]=0;
-                    }
+        if(s3.length()!=s1.length()+s2.length()) return 0;
+        int n=s1.length();
+        int m=s2.length();
+        bool dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0&&j==0) dp[i][j]=1;
+                else if(i==0) dp[i][j]=(dp[i][j-1]&&s3[i+j-1]==s2[j-1]);
+                else if(j==0) dp[i][j]=(dp[i-1][j]&&s3[i+j-1]==s1[i-1]);
+                else{
+                    dp[i][j]=(dp[i][j-1]&&s3[i+j-1]==s2[j-1]);
+                    dp[i][j]|=(dp[i-1][j]&&s3[i+j-1]==s1[i-1]);
                 }
             }
         }
-        return dp[l3][l1][l2];
+        return dp[n][m];
     }
 };
