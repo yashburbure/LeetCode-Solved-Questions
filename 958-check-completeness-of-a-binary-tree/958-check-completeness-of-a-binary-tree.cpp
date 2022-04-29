4fbea1121bb4b1b8bebbbc28eea32ad97ee62ae0
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-04-30 01:36:22 +0530

    Time: 7 ms (50.47%), Space: 10.3 MB (92.86%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.cpp
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.cpp
@@ -11,50 +11,26 @@
  */
 class Solution {
 public:
-    int find_ht(TreeNode* root){
-        int lh=0;
-        int rh=0;
-        if(root->left) lh=1+find_ht(root->left);
-        if(root->right) rh=1+find_ht(root->right);
-        return max(lh,rh);
-    }
     bool isCompleteTree(TreeNode* root) {
-        int height=find_ht(root);
-        int curr_level=0;
+        bool flag=1;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
-            int n=q.size();
-            if(curr_level>=height-1){
-                int flag=1;
-                for(int i=0;i<n;i++){
-                    TreeNode* curr=q.front();
-                    q.pop();
-                    if(flag){
-                        if(curr->left && curr->right){
-                            flag=1;
-                        }
-                        else if(curr->left) flag=0;
-                        else if(curr->right) return 0;
-                        else flag=0;
-                    }
-                    else{
-                        if(curr->left || curr->right) return 0;
-                    }
-                }
+            TreeNode* curr=q.front();
+            q.pop();
+            if(curr==NULL){
+                flag=0;
             }
             else{
-                for(int i=0;i<n;i++){
-                    TreeNode* curr=q.front();
-                    q.pop();
-                    if(curr->left && curr->right){
-                        q.push(curr->left);
-                        q.push(curr->right);
-                    }
-                    else return 0;
+                if(flag){
+                    q.push(curr->left);
+                    q.push(curr->right);
+                }
+                else{
+                    if(curr) return 0;
                 }
             }
-            curr_level++;
+            
         }
         return 1;
     }
