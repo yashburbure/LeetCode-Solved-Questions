eca4216d659f2e40163a2df85130990a1962840e
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-05-05 08:33:29 +0530

    Time: 76 ms (65.65%), Space: 16.3 MB (95.93%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -3,25 +3,24 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int dp[m+1][n+1];
+        int previous[n+1];
+        for(int i=0;i<=n;i++) previous[i]=0;
+        int dp[n+1];
         int ans=0;
-        for(int i=0;i<=m;i++){
+        for(int i=1;i<=m;i++){
             for(int j=0;j<=n;j++){
-                if(i==0||j==0) dp[i][j]=0;
+                if(j==0) dp[j]=0;
                 else{
-                    if(matrix[i-1][j-1]=='1'){
-                        dp[i][j]=1;
-                        if(dp[i-1][j-1]){
-                            int ele=sqrt(min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]}));
-                            dp[i][j]=(ele+1)*(ele+1);
-                        }
-                    }
+                    if(matrix[i-1][j-1]=='0') dp[j]=0;
                     else{
-                        dp[i][j]=0;
+                        dp[j]=1;
+                        int ele=sqrt(min({previous[j-1],previous[j],dp[j-1]}));
+                        dp[j]=(ele+1)*(ele+1);
                     }
                 }
-                ans=max(ans,dp[i][j]);
+                ans=max(ans,dp[j]);
             }
+            for(int j=0;j<=n;j++) previous[j]=dp[j];
         }
         return ans;
     }
