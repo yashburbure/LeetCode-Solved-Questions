e121e5e5e60ad36cf0b02a763f3aac7efc691bf5
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-05-10 17:55:51 +0530

    Time: 10 ms (5.87%), Space: 6.8 MB (28.79%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void find_ans(vector<vector<int>>& ans,int sum,int no_used,vector<int> curr,int last_ele,int k){
+    void find_ans(vector<vector<int>>& ans,int sum,int no_used,vector<int> &curr,int last_ele,int &k){
         if(sum==0 && no_used==k){
             ans.push_back(curr);
             return ;
@@ -16,7 +16,8 @@ public:
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
-        find_ans(ans,n,0,{},0,k);
+        vector<int> curr;
+        find_ans(ans,n,0,curr,0,k);
         return ans;
     }
 };
