da0209ba0a7c9549750cdf622af305fe8da1d15f
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-05-12 18:41:04 +0530

    Time: 13 ms (31.60%), Space: 6.1 MB (61.79%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool is_safe(int &m,int &n,vector<string>& board){
+        int size=board.size();
+        for(int i=m-1;i>=0;i--){
+            if(board[i][n]=='Q') return 0;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(board[m][i]=='Q') return 0;
+        }
+        for(int i=m-1,j=n-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q') return 0;
+        }
+        for(int i=m-1,j=n+1;i>=0 && j<size;i--,j++){
+            if(board[i][j]=='Q') return 0;
+        }
+        return 1;
+    }
+    void back_trap(int &ans,int &n,int &row,vector<string>& board){
+        if(row==n){
+            ans++;
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(is_safe(row,i,board)){
+                board[row][i]='Q';
+                row++;
+                back_trap(ans,n,row,board);
+                row--;
+            }
+            board[row][i]='.';
+        }
+    }
+    int totalNQueens(int n) {
+        int ans=0;
+        vector<string> board(n,string(n,'.'));
+        int row=0;
+        back_trap(ans,n,row,board);
+        return ans;
+    }
+};
