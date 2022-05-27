e9951e3272d5b6263d233f7066e0eb485321ef9b
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-05-27 17:52:33 +0530

    Time: 1425 ms (12.68%), Space: 59.6 MB (35.05%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -24,27 +24,33 @@ public:
             }
         }
     }
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
+    int minCostConnectPoints(vector<vector<int>>& pt) {
+        int n=pt.size();
+        vector<pair<int,int>> points;
+        for(auto &it:pt){
+            points.push_back({it[0],it[1]});
+        }
         int wt;
         vector<pair<int,pair<int,int>>> edges;
         for(auto &it:points){
-            make({it[0],it[1]});
+            make({it.first,it.second});
         }
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                wt=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                wt=abs(points[i].first-points[j].first)+abs(points[i].second-points[j].second);
                 edges.push_back({wt,{i,j}});
             }
         }
         int ans=0;
-        sort(edges.begin(),edges.end(),[&](auto a,auto b){
+        sort(edges.begin(),edges.end(),[&](auto& a,auto& b){
             return a.first<b.first;
         });
         for(auto &it:edges){
             int wt=it.first;
-            pair<int,int> pt1={points[it.second.first][0],points[it.second.first][1]};
-            pair<int,int> pt2={points[it.second.second][0],points[it.second.second][1]};
+            pair<int,int> pt1=
+                {points[it.second.first].first,points[it.second.first].second};
+            pair<int,int> pt2=
+                {points[it.second.second].first,points[it.second.second].second};
             if(find(pt1)!=find(pt2)){
                 ans+=wt;
                 Union(pt1,pt2);
