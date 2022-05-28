b6b42c2683577121b5292fea7ff45204a7538a1d
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-05-28 21:29:20 +0530

    Time: 417 ms (58.56%), Space: 67 MB (34.45%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,18 +1,18 @@
+const int k=1e6+7;
 class Solution {
 public:
-    map<pair<int,int>,pair<int,int>> parent;
-    map<pair<int,int>,int> sz;
-    void make(pair<int,int> v){
+    int parent[k],sz[k];
+    void make(int v){
         parent[v]=v;
         sz[v]=1;
     }
-    pair<int,int> find(pair<int,int> &v){
+    int find(int &v){
         if(parent[v]==v) return v;
         return parent[v]=find(parent[v]);
     }
-    void Union(pair<int,int> &u,pair<int,int> &v){
-        auto a=find(u);
-        auto b=find(v);
+    void Union(int &u,int &v){
+        int a=find(u);
+        int b=find(v);
         if(a!=b){
             if(sz[a]<sz[b]){
                 parent[a]=b;
@@ -32,12 +32,13 @@ public:
         }
         int wt;
         vector<pair<int,pair<int,int>>> edges;
-        for(auto &it:points){
-            make({it.first,it.second});
+        for(int i=0;i<n;i++){
+            make(i);
         }
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                wt=abs(points[i].first-points[j].first)+abs(points[i].second-points[j].second);
+                wt=abs(points[i].first-points[j].first)
+                    +abs(points[i].second-points[j].second);
                 edges.push_back({wt,{i,j}});
             }
         }
@@ -47,10 +48,8 @@ public:
         });
         for(auto &it:edges){
             int wt=it.first;
-            pair<int,int> pt1=
-                {points[it.second.first].first,points[it.second.first].second};
-            pair<int,int> pt2=
-                {points[it.second.second].first,points[it.second.second].second};
+            int pt1=it.second.first;
+            int pt2=it.second.second;
             if(find(pt1)!=find(pt2)){
                 ans+=wt;
                 Union(pt1,pt2);
