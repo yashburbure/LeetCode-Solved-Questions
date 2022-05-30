9927f4939c2b0ad5f5d5b59211754688ae0f2316
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-05-30 10:26:43 +0530

    Time: 3 ms (67.24%), Space: 5.9 MB (76.09%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         long long ans=(1ll*dividend)/(1ll*divisor);
-        cout<<ans<<endl;
         if(ans>INT_MAX) ans=INT_MAX;
         if(ans<INT_MIN) ans=INT_MIN;
         return ans;
