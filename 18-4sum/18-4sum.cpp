1230a4e44edb674aed967e62bcc76aadd74aece5
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-06-04 09:26:09 +0530

    Time: 608 ms (11.53%), Space: 49.2 MB (17.06%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
         vector<vector<int>> ans;
-        vector<tuple<int,int,int,int>> pos_ans;
+        set<vector<int>> st;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
@@ -12,7 +12,7 @@ public:
                 while(lo<hi){
                     long long sum=0ll+nums[lo]+nums[hi]+nums[i]+nums[j];
                     if(sum==target){
-                        pos_ans.push_back({nums[i],nums[j],nums[lo],nums[hi]});
+                        st.insert({nums[i],nums[j],nums[lo],nums[hi]});
                         hi--;
                         lo++;
                     }
@@ -20,10 +20,6 @@ public:
                     else lo++;
                 }
             }
-        }
-        set<vector<int>> st;
-        for(auto &it:pos_ans){
-            st.insert({get<0>(it),get<1>(it),get<2>(it),get<3>(it)});
         }
         for(auto &it:st){
             ans.push_back(it);
