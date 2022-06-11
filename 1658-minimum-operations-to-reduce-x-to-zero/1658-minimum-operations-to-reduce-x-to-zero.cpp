b7df8bf2896a31b65fc7ce1f20c359774f4a6c45
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-06-11 11:33:13 +0530

    Time: 373 ms (44.74%), Space: 103.1 MB (48.70%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int &x) {
         int n=nums.size();
-        int* prefix=new int[n];
         int* suffix=new int[n];
         int ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(i==0) prefix[i]=nums[i];
-            else prefix[i]=prefix[i-1]+nums[i];
-            if(prefix[i]==x) ans=min(ans,i+1);
-        }
         for(int i=n-1;i>=0;i--){
             if(i==n-1) suffix[i]=nums[i];
             else suffix[i]=suffix[i+1]+nums[i];
             if(suffix[i]==x) ans=min(ans,n-i);
         }
+        int previous_sum=0;
+        int lo,hi,mid;
         for(int i=0;i<n-1;i++){
-            int lo=i+1;
-            int hi=n-1;
-            int mid;
+            previous_sum+=nums[i];
+            lo=i+1;
+            hi=n-1;
             while(hi-lo>1){
                 mid=(lo+hi)/2;
-                if(suffix[mid]+prefix[i]>x) lo=mid+1;
+                if(suffix[mid]+previous_sum>x) lo=mid+1;
                 else hi=mid;
             }
-            
-            if(prefix[i]+suffix[lo]==x) ans=min(ans,i+1+n-lo);
-            if(prefix[i]+suffix[hi]==x) ans=min(ans,i+1+n-hi);
+            if(previous_sum==x) ans=min(ans,i+1);
+            if(previous_sum+suffix[lo]==x) ans=min(ans,i+1+n-lo);
+            if(previous_sum+suffix[hi]==x) ans=min(ans,i+1+n-hi);
         }
+        delete[] suffix;
+        previous_sum+=nums[n-1];
+        if(previous_sum==x) ans=min(ans,n);
         return (ans==INT_MAX)?-1:ans;
     }
 };
