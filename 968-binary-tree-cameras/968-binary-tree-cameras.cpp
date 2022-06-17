9029a5487ef0bdd259073f0c3afee27ce5ce1470
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-06-17 23:57:25 +0530

    Time: 111 ms (5.10%), Space: 39.4 MB (5.10%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -11,34 +11,36 @@
  */
 class Solution {
 public:
-    int dfs(TreeNode*& root,bool parent,bool pcol,string path,
+    int dfs(TreeNode*& root,bool parent,bool pcol,string& path,
              map<pair<string,pair<bool,bool>>,int>& dp){
         if(!root){
             return 0;
         }
         if(dp.find({path,{parent,pcol}})!=dp.end()) return dp[{path,{parent,pcol}}];
+        string s1=path+'L';
+        string s2=path+'R';
         int ans=1e8;
         if(parent){
             if(pcol){
-                ans=min(ans,dfs(root->left,1,0,path+'L',dp)+dfs(root->right,1,0,path+'R',dp));
-                ans=min(ans,1+dfs(root->left,1,1,path+'L',dp)+dfs(root->right,1,1,path+'R',dp));
+                ans=min(ans,dfs(root->left,1,0,s1,dp)+dfs(root->right,1,0,s2,dp));
+                ans=min(ans,1+dfs(root->left,1,1,s1,dp)+dfs(root->right,1,1,s2,dp));
             }
             else{
-                ans=min(ans,1+dfs(root->left,1,1,path+'L',dp)+dfs(root->right,1,1,path+'R',dp));
+                ans=min(ans,1+dfs(root->left,1,1,s1,dp)+dfs(root->right,1,1,s2,dp));
                 if(root->left && root->right){
-                    ans=min(ans,dfs(root->left,0,0,path+'L',dp)+dfs(root->right,1,0,path+'R',dp));
-                    ans=min(ans,dfs(root->left,1,0,path+'L',dp)+dfs(root->right,0,0,path+'R',dp));
+                    ans=min(ans,dfs(root->left,0,0,s1,dp)+dfs(root->right,1,0,s2,dp));
+                    ans=min(ans,dfs(root->left,1,0,s1,dp)+dfs(root->right,0,0,s2,dp));
                 }
                 else if(root->left){
-                    ans=min(ans,dfs(root->left,0,0,path+'L',dp));
+                    ans=min(ans,dfs(root->left,0,0,s1,dp));
                 }
                 else if(root->right){
-                    ans=min(ans,dfs(root->right,0,0,path+'R',dp));
+                    ans=min(ans,dfs(root->right,0,0,s2,dp));
                 }
             }
         }
         else{
-            ans=min(ans,1+dfs(root->left,1,1,path+'L',dp)+dfs(root->right,1,1,path+'R',dp));
+            ans=min(ans,1+dfs(root->left,1,1,s1,dp)+dfs(root->right,1,1,s2,dp));
         }
         return dp[{path,{parent,pcol}}]=ans;
     }
