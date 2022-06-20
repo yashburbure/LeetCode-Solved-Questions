1a2ad010aaad646a8d2800cf861d8372d8024581
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-06-21 00:00:25 +0530

    Time: 81 ms (67.82%), Space: 75.3 MB (19.31%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -8,28 +8,30 @@ class Trie{
             }   
         }
 };
-void insert(Trie* root,string& s,int i,int ind){
+void insert(Trie*& root,string& s,int& i,int& ind){
     if(i==s.length()-1){
         root->ind.push_back(ind);
         return;
     }
-    if(root->child[s[i+1]-'a']){
-        insert(root->child[s[i+1]-'a'],s,i+1,ind);
+    i++;
+    if(root->child[s[i]-'a']){
+        insert(root->child[s[i]-'a'],s,i,ind);
     }
     else{
         Trie* nn=new Trie();
-        root->child[s[i+1]-'a']=nn;
-        insert(root->child[s[i+1]-'a'],s,i+1,ind);
+        root->child[s[i]-'a']=nn;
+        insert(root->child[s[i]-'a'],s,i,ind);
     }
 }
-void traverse(Trie* root,string s,int i,vector<bool>& vis){
+void traverse(Trie*& root,string& s,int i,vector<bool>& vis){
     for(auto &it:root->ind){
         vis[it]=1;
     }
     if(i==s.length()-1){
         return;
     }
-    traverse(root->child[s[i+1]-'a'],s,i+1,vis);
+    i++;
+    traverse(root->child[s[i]-'a'],s,i,vis);
 }
 class Solution {
 public:
@@ -40,16 +42,17 @@ public:
         sort(words.begin(),words.end(),[&](string& a,string& b){
             return a.length()>b.length();
         });
+        int temp;
         for(int i=0;i<n;i++){
             reverse(words[i].begin(),words[i].end());
-            
+            temp=0;
             if(root->child[words[i][0]-'a']){
-                insert(root->child[words[i][0]-'a'],words[i],0,i);
+                insert(root->child[words[i][0]-'a'],words[i],temp,i);
             }
             else{
                 Trie* nn=new Trie();
                 root->child[words[i][0]-'a']=nn;
-                insert(root->child[words[i][0]-'a'],words[i],0,i);
+                insert(root->child[words[i][0]-'a'],words[i],temp,i);
             }
         }
         vector<bool> vis(n,0);
@@ -57,7 +60,8 @@ public:
         for(int i=0;i<n;i++){
             if(vis[i]) continue;
             ans+=words[i].length()+1;
-            traverse(root->child[words[i][0]-'a'],words[i],0,vis);
+            temp=0;
+            traverse(root->child[words[i][0]-'a'],words[i],temp,vis);
         }
         return ans;
     }
