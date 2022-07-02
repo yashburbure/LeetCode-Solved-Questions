81f10218ab48c39f955ab965b14528411498a3b6
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-03 02:28:23 +0530

    Time: 12 ms (86.98%), Space: 10.5 MB (60.39%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<pair<int,int>> narr(n);
+        for(int i=0;i<n;i++){
+            narr[i]={nums[i],i};
+        }
+        sort(narr.begin(),narr.end());
+        int hi=n-1;
+        int lo=0;
+        int sum;
+        while(hi>lo){
+            sum=narr[hi].first+narr[lo].first;
+            if(sum==target) return {narr[lo].second,narr[hi].second};
+            else if(sum>target) hi--;
+            else lo++;
+        }
+        vector<int> ans;
+        return ans;
+    }
+};
