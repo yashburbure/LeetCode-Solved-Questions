84428c94618269b299cf90fc4ef81c497f02e98d
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-19 23:05:46 +0530

    Time: 33 ms (54.70%), Space: 14.9 MB (21.55%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool canFinish(int nc, vector<vector<int>>& pre) {
-        vector<set<int>> graph1(nc);
-        vector<int> graph2[nc];
+    bool canFinish(int &nc, vector<vector<int>>& pre) {
+        vector<set<short int>> graph1(nc);
+        vector<short int> graph2[nc];
         for(auto &it:pre){
             graph1[it[0]].insert(it[1]);
             graph2[it[1]].push_back(it[0]);
         }
-        queue<int> q;
+        queue<short int> q;
         vector<bool> vis(nc,0);
-        for(int i=0;i<nc;i++){
+        for(short int i=0;i<nc;i++){
             if(graph1[i].size()==0){
                 q.push(i);
                 vis[i]=1;
             }
         }
+        short int tp;
         while(q.size()){
-            int tp=q.front();
+            tp=q.front();
             q.pop();
-            for(auto &it:graph2[tp]){
+            for(short int &it:graph2[tp]){
                 graph1[it].erase(graph1[it].lower_bound(tp));
                 if(!graph1[it].size()){
                     vis[it]=1;
@@ -26,7 +27,7 @@ public:
                 } 
             }
         }
-        for(int i=0;i<nc;i++){
+        for(short int i=0;i<nc;i++){
             if(!vis[i]) return false;
         }
         return true;
