5550d2b2609b945c5169c2880789024aea43f72d
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-25 13:56:10 +0530

    Time: 41 ms (11.93%), Space: 18.7 MB (49.59%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -4,19 +4,15 @@ public:
         int n=citations.size();
         int hi,lo,mid;
         int ans=-1;
-        unordered_map<int,int> freq;
-        for(int i=0;i<n;i++){
-            freq[citations[i]]++;
-        }
+        int papers_g,papers_l,papers_e,papers_n;
         for(int i=0;i<=1000;i++){
             auto it1=upper_bound(citations.begin(),citations.end(),i)-citations.begin();
             auto it2=lower_bound(citations.begin(),citations.end(),i)-citations.begin();
-            int papers_g=n-it1;
-            int papers_l=it2;
-            int papers_e=freq[i];
+            papers_g=n-it1;
+            papers_l=it2;
+            papers_e=n-papers_g-papers_l;
             if(papers_g>i) continue;
-            // cout<<papers_g<<" "<<papers_l<<" "<<papers_e<<endl;
-            int papers_n=i-papers_g;
+            papers_n=i-papers_g;
             if(papers_n<=papers_e) ans=max(ans,i);
         }
         return ans;
