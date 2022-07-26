ffda8fe45fdeaf84ef3a8e39e1a9fe8077991248
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-26 12:40:39 +0530

    Time: 60 ms (99.70%), Space: 24.9 MB (91.52%) - LeetHub

---
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        vector<pair<int,int>> arr;
+        int n=intervals.size();
+        for(int i=0;i<n;i++){
+            arr.push_back({intervals[i][0],i});
+        }
+        sort(arr.begin(),arr.end());
+        int lo,hi,mid;
+        vector<int> RightInterval(n);
+        for(int i=0;i<n;i++){
+            lo=0;
+            hi=n-1;
+            while(hi-lo>1){
+                mid=(hi+lo)/2;
+                if(arr[mid].first>=intervals[i][1]){
+                    hi=mid;
+                }
+                else{
+                    lo=mid+1;
+                }
+            }
+            if(arr[lo].first>=intervals[i][1]){
+                RightInterval[i]=arr[lo].second;
+            }
+            else if(arr[hi].first>=intervals[i][1]){
+                RightInterval[i]=arr[hi].second;
+            }
+            else{
+                RightInterval[i]=-1;
+            }
+        }
+        return RightInterval;
+    }
+};
