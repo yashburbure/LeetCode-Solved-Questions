7906d988980264923b15dffd297d54872ac8b98b
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-28 09:31:10 +0530

    Time: 4 ms (95.45%), Space: 7.2 MB (94.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool isAnagram(string& s, string& t) {
         int i;
-        vector<int> freq_s(256,0),freq_t(256,0);
+        vector<int> freq_s(127,0),freq_t(127,0);
         for(i=0;i<s.length();i++){
             freq_s[s[i]]++;
         }
         for(i=0;i<t.length();i++){
             freq_t[t[i]]++;
         }
-        for(i=0;i<256;i++){
+        for(i=0;i<127;i++){
             if(freq_s[i]!=freq_t[i]) return false;
         }
         return true;
