5d5756652440a97bf7b0e438a20d1b040e850cce
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-28 09:29:10 +0530

    Time: 8 ms (82.77%), Space: 7.1 MB (94.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
+    bool isAnagram(string& s, string& t) {
+        int i;
+        vector<int> freq_s(256,0),freq_t(256,0);
+        for(i=0;i<s.length();i++){
+            freq_s[s[i]]++;
+        }
+        for(i=0;i<t.length();i++){
+            freq_t[t[i]]++;
+        }
+        for(i=0;i<256;i++){
+            if(freq_s[i]!=freq_t[i]) return false;
+        }
+        return true;
     }
 };
