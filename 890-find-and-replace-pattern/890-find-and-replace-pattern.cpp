84005a7efc008a9e56a05374aba7048aed11e56b
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-29 13:10:34 +0530

    Time: 7 ms (55.39%), Space: 8.4 MB (65.33%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string& pattern) {
         vector<string> ans;
-        map<char,char> mp1,mp2;
+        unordered_map<char,char> mp1,mp2;
         bool flag;
-        int i;
+        short int i;
         for(string& it:words){
             flag=true;
             for(i=0;i<pattern.length();i++){
                 if(mp1.find(pattern[i])!=mp1.end()){
-                    // cout<<"hello1"<<endl;
                     if(mp1[pattern[i]]!=it[i]){
                         flag=false;
                     }
                 }
                 else if(mp2.find(it[i])!=mp2.end()){
-                    // cout<<"hello2"<<endl;
                     flag=false;
                 }
                 else{
-                    // cout<<"hello3"<<endl;
                     if(mp2.find(it[i])!=mp2.end()){
                         flag=false;
                     }
