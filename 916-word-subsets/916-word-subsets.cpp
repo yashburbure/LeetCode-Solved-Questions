db8394d55addc34bd4809b4ebf4ceaaa892dcc3f
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-30 10:31:20 +0530

    Time: 107 ms (99.78%), Space: 57.2 MB (98.65%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    void reset(vector<int>& Hsh){
-        for(int i=0;i<26;i++) Hsh[i]=0;
-    }
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int n1=words1.size();
         int n2=words2.size();
         int i,j;
         vector<string> Subsets;
-        vector<int> Hsh_Max(26,0);
-        vector<int> Hsh(26,0);
+        int Hsh[26]={0},Hsh_Max[26]={0};
         for(i=0;i<n2;i++){
             for(j=0;j<words2[i].size();j++){
                 Hsh[words2[i][j]-'a']++;
@@ -17,7 +15,7 @@ public:
             for(j=0;j<26;j++){
                 Hsh_Max[j]=max(Hsh_Max[j],Hsh[j]);
             }
-            reset(Hsh);
+            memset(Hsh,0,sizeof(Hsh));
         }
         for(i=0;i<n1;i++){
             for(j=0;j<words1[i].size();j++){
@@ -32,7 +30,7 @@ public:
             if(flag){
                 Subsets.push_back(words1[i]);
             }
-            reset(Hsh);
+            memset(Hsh,0,sizeof(Hsh));
         }
         return Subsets;
     }
