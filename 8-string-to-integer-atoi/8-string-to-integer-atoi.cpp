4c3c22a7c7121956a207cffd52fd7818d85ad821
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-07-30 20:53:47 +0530

    Time: 3 ms (83.04%), Space: 7.1 MB (53.78%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        char ch=' ';
+        int i=0;
+        long long ans=0;
+        while(s[i]==' '){
+            i++;
+        }
+        if(s[i]=='+' || s[i]=='-'){
+            ch=s[i];
+            i++;
+        }
+        for(i=i;i<s.length() && (s[i]>='0' && s[i]<='9');i++){
+            ans=(10ll*ans)+s[i]-'0';
+            if(ans>=INT_MAX) break;
+        }
+        if(ch=='-'){
+            ans=-ans;
+            if(ans<INT_MIN) ans=INT_MIN;
+        }
+        else{
+            if(ans>INT_MAX) ans=INT_MAX;
+        }
+        return ans;
+    }
+};
