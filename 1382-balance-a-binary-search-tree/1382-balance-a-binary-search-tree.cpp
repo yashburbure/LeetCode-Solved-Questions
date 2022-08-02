b2a2208b9b8a59856ef9416d4719c08266c091b3
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-08-02 22:16:48 +0530

    Time: 258 ms (37.32%), Space: 63.4 MB (33.73%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     void inorder(TreeNode*& root,vector<int>& InorderArray){
@@ -17,19 +6,22 @@ public:
         InorderArray.push_back(root->val);
         inorder(root->right,InorderArray);
     }
-    TreeNode* buildBST(int st,int end,vector<int>& InorderArray){
+    TreeNode* buildBST(int &st,int &end,vector<int>& InorderArray){
         if(st>end){
             return NULL;
         }
         int mid=(st+end)/2;
         TreeNode* nn=new TreeNode(InorderArray[mid]);
-        nn->left=buildBST(st,mid-1,InorderArray);
-        nn->right=buildBST(mid+1,end,InorderArray);
+        mid--;
+        nn->left=buildBST(st,mid,InorderArray);
+        mid+=2;
+        nn->right=buildBST(mid,end,InorderArray);
         return nn;
     }
     TreeNode* balanceBST(TreeNode*& root) {
         vector<int> InorderArray;
         inorder(root,InorderArray);
-        return buildBST(0,InorderArray.size()-1,InorderArray);
+        int st=0,end=InorderArray.size()-1;
+        return buildBST(st,end,InorderArray);
     }
 };
