cbc55236b20793e1ed59baed6bdb67d305f62363
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-08-05 21:29:15 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (88.98%) - LeetHub

--- 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
-        vector<vector<int>> AllLevel;
-        for(int i=1;;i++){
-            vector<int> CurrLevel;
-            bool flag=false;
-            int start=(1ll<<(i-1));
-            int end=(1ll<<(i))-1;
-            for(int j=start;j<=end;j++){
-                if(j==label) flag=true;
-                CurrLevel.push_back(j);
-            }
-            if(i%2==0){
-                reverse(CurrLevel.begin(),CurrLevel.end());
-            }
-            AllLevel.push_back(CurrLevel);
-            if(flag) break;
-        }
-        int index=-1;
-        for(int i=1;i<AllLevel[AllLevel.size()-1].size();i++){
-            if(AllLevel[AllLevel.size()-1][i]==label){
-                index=i;
-                break;
-            }
-        }
-        int CurrLevel=AllLevel.size()-1;
         vector<int> ZigZagPath;
-        int element=label;
-        while(element!=1){
-            ZigZagPath.push_back(element);
-            index=index/2;
-            CurrLevel--;
-            element=AllLevel[CurrLevel][index];
+        int temp=label;
+        int ct=0;
+        while(label!=1){
+            ct++;
+            ZigZagPath.push_back(label);
+            int NumberOfBits=log2(label);
+            int start=(1ll<<NumberOfBits);
+            int end=(1ll<<(NumberOfBits+1))-1;
+            int mid=(start+end)/2;
+            if(label<=mid){
+                int positionFromstart=label-start;
+                int oppositeElement=end-positionFromstart;
+                label=oppositeElement/2;
+            }
+            else{
+                int positionFromend=end-label;
+                int oppositeElement=start+positionFromend;
+                label=oppositeElement/2;
+            }
         }
         ZigZagPath.push_back(1);
         reverse(ZigZagPath.begin(),ZigZagPath.end());
