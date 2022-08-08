62e0d85e11e5e0a5f929b3ebb8f18434413bac34
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-08-08 19:18:04 +0530

    Time: 14 ms (85.19%), Space: 10.6 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -5,8 +5,9 @@ public:
         vector<int> lis(n+1,INT_MAX);
         lis[0]=INT_MIN;
         int ans=0;
+        int it;
         for(int i=0;i<n;i++){
-            int it=lower_bound(lis.begin(),lis.end(),nums[i])-lis.begin();
+            it=lower_bound(lis.begin(),lis.end(),nums[i])-lis.begin();
             ans=max(ans,it);
             if(nums[i]<lis[it] && lis[it-1]<nums[i]){
                 lis[it]=nums[i];
