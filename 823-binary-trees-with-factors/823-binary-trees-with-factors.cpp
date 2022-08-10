007d7dc3567e81a351ece00d1123f589cd7440b1
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-08-10 13:08:28 +0530

    Time: 2695 ms (5.23%), Space: 25.3 MB (5.85%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -2,28 +2,21 @@ class Solution {
 public:
     int numFactoredBinaryTrees(vector<int>& arr) {
         sort(arr.begin(),arr.end());
-        unordered_map<int,long int> freq;
-        long int ans=0,curr_ans;
-        const long int M=1e9+7;
-        long long temp;
+        unordered_map<int,int> freq;
+        int ans=0,curr_ans;
+        const int M=1e9+7;
+        int ct,temp;
         for(int i=0;i<arr.size();i++){
-            ans=(ans+1)%M;
-            freq[arr[i]]=1;
+            ct=1;
             for(int j=2;j<=sqrt(arr[i]);j++){
                 if(arr[i]%j==0){
-                    if(arr[i]/j!=j){
-                        temp=(freq[j]*1ll*freq[arr[i]/j])%M;
-                        temp=(2*temp)%M;
-                        ans=(ans+temp)%M;
-                        freq[arr[i]]=(freq[arr[i]]+temp)%M;
-                    }
-                    else{
-                        temp=(freq[j]*1ll*freq[arr[i]/j])%M;
-                        ans=(ans+temp)%M;
-                        freq[arr[i]]=(freq[arr[i]]+temp)%M;
-                    }
+                    temp=(freq[j]*1ll*freq[arr[i]/j])%M;
+                    if(j!=arr[i]/j) temp=(2ll*temp)%M;
+                    ct=(ct+0ll+temp)%M;
                 }
             }
+            ans=(ans+0ll+ct)%M;
+            freq[arr[i]]=ct;
         }
         return ans;
     }
