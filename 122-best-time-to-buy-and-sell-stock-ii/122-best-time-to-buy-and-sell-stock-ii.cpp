58e5e8ed1e5e805897dc5fc917054dbafcb16988
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-08-21 23:20:55 +0530

    Time: 12 ms (51.37%), Space: 13.3 MB (24.63%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,13 +18,10 @@ public:
         int n=nums.size();
         dp[0][0]=0;
         dp[0][1]=-nums[0];
-        int ans=0;
         for(int i=1;i<n;i++){
             dp[i][1]=max(dp[i-1][1],-nums[i]+dp[i-1][0]);
             dp[i][0]=max(dp[i-1][0],nums[i]+dp[i-1][1]);
-            ans=max(ans,dp[i][0]);
-            ans=max(ans,dp[i][1]);
         }
-        return ans;
+        return max(dp[n-1][0],dp[n-1][1]);
     }
 };
