7028feb348f5acd3e9780b277298d21f5b760f8b
yashburbure <94693548+yashburbure@users.noreply.github.com>
2022-08-26 13:53:31 +0530

    Time: 4 ms (61.58%), Space: 6.6 MB (24.29%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    vector<int> posSet;
-    void generateSet(int freq[],int no,int ct){
-        if(ct==0){
-            posSet.push_back(no);
-            return;
-        }
-        for(int i=0;i<10;i++){
-            if(no==0 && i==0) continue;
-            if(freq[i]){
-                freq[i]--;
-                generateSet(freq,no*10+i,ct-1);
-                freq[i]++;
-            }
-        }
-    }
     bool reorderedPowerOf2(int n) {
-        int freq[10]={0};
-        int temp=n;
-        int ct=0;
-        while(temp){
-            ct++;
-            int rem=temp%10;
-            freq[rem]++;
-            temp/=10;
+        map<string,bool> powTwo;
+        long long no=1;
+        string s;
+        while(no<=1e9){
+            s=to_string(no);
+            sort(s.begin(),s.end());
+            powTwo[s]=true;
+            no=no*2;
         }
-        generateSet(freq,0,ct);
-        for(auto &it:posSet){
-            while(it%2==0) it=it/2;
-            if(it==1) return true;
-        }
-        return false;
+        s=to_string(n);
+        sort(s.begin(),s.end());
+        return powTwo[s];
     }
 };
